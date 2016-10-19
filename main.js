e0838207d7a5152b86d9771f09c19f31f8f635a5
Steven McKeown <SMcKeown@HME.COM>
2016-10-19 16:32:55 -0400

    missed pushing these

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log('Got request for ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>This is a website!</h1>');
-}).listen(process.env.PORT);
+/*
+ * Module dependencies
+ */
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+
+
+var app = express()
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
+})
+
+app.listen(3000)
